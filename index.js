72df7c31b4b32e6bc3088819c1b897bf67b64f9a
Yegeta <149103185+Yegetaa@users.noreply.github.com>
2023-12-12 16:13:32 -0500

    button elements created

--- index.js
+++ index.js
@@ -1,43 +1,53 @@
-
 document.addEventListener('DOMContentLoaded', function () {
-// Elements
-const contentDiv = document.getElementById('content');
-const gifDisplay = document.getElementById('gifDisplay');
-const prevButton = document.getElementById('prevButton');
-const nextButton = document.getElementById('nextButton');
-
-// the Gifs
-const gifs = [
-    "gifs/gif1.gif",
-    "gifs/gif2.gif",
-    "gifs/gif3.gif",
-    "gifs/gif4.gif",
-    "gifs/gif5.gif",
-];
-
-
-let currentGifIndex = 0;
-
-// Function to update the displayed GIF
+    // Elements
+    const contentDiv = document.getElementById('content');
+    const gifDisplay = document.getElementById('gifDisplay');
+    const buttonDiv = document.getElementById('buttons');
+  
+    // Create Previous and Next buttons
+    const prevButton = document.createElement('button');
+    prevButton.textContent = 'Previous';
+  
+    const nextButton = document.createElement('button');
+    nextButton.textContent = 'Next';
+  
+    // The Gifs
+    const gifs = [
+      "gifs/gif1.gif",
+      "gifs/gif2.gif",
+      "gifs/gif3.gif",
+      "gifs/gif4.gif",
+      "gifs/gif5.gif",
+    ];
+  
+    let currentGifIndex = 0;
+  
+    // Function to update the displayed GIF
     function updateGif() {
-        gifDisplay.src = gifs[currentGifIndex];
+      gifDisplay.src = gifs[currentGifIndex];
     }
-
-// Event listeners and handlers
-prevButton.addEventListener('click', function () {
-    if (currentGifIndex > 0) {
+  
+    // Event listeners and handlers
+    prevButton.addEventListener('click', function () {
+      if (currentGifIndex > 0) {
         currentGifIndex--;
         updateGif();
-    }
-});
-
-nextButton.addEventListener('click', function () {
-    if (currentGifIndex < gifs.length - 1) {
+        prevButton.style.backgroundColor = 'lightgray';
+      }
+    });
+  
+    nextButton.addEventListener('click', function () {
+      if (currentGifIndex < gifs.length - 1) {
         currentGifIndex++;
         updateGif();
-    }
-});
-
-
-updateGif();
-});
+        nextButton.style.backgroundColor = 'gray';
+      }
+    });
+  
+    // Append buttons to buttonDiv
+    buttonDiv.appendChild(prevButton);
+    buttonDiv.appendChild(nextButton);
+  
+    updateGif();
+  });
+  
